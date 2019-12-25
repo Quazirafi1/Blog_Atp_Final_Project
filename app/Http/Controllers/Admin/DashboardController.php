d2b115d8f87e49df98c9cc5cd4866dc4802e8553
Rafi <quazirafi1@gmail.com>
2019-12-25 17:55:56 +0600

    Dashboard hadnled for admin and authors

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
+use App\Post;
+use App\Tag;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view ('admin.dashboard');
+    public function index()
+    {
+        $posts = Post::all();
+        $popular_posts = Post::withCount('comments')
+            ->withCount('favorite_to_users')
+            ->orderBy('view_count','desc')
+            ->orderBy('comments_count','desc')
+            ->orderBy('favorite_to_users_count','desc')
+            ->take(5)->get();
+        $total_pending_posts = Post::where('is_approved',false)->count();
+        $all_views = Post::sum('view_count');
+        $author_count = User::where('role_id',2)->count();
+        $category_count = Category::all()->count();
+        $tag_count = Tag::all()->count();
+
+        return view('admin.dashboard',compact('posts','popular_posts','total_pending_posts','all_views','author_count','new_authors_today','active_authors','category_count','tag_count'));
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -141,18 +141,18 @@
                                                 <a href="javascript:void(0);" onclick="toastr.info('Must Be Authenticated User To Access This Feature','Information',{
                                                     closeButton: true,
                                                     progressBar: true,
-                                                })"><i class="ion-heart"></i>{{ $post->favorite_to_users->count() }}</a>
+                                                })"><i class="ion-heart"></i>{{ $randompost->favorite_to_users->count() }}</a>
                                             @else
                                                 <a href="javascript:void(0);" onclick="document.getElementById('favorite-form-{{ $post->id }}').submit();"
-                                                   class="{{ !Auth::user()->favorite_posts->where('pivot.post_id',$post->id)->count()  == 0 ? 'favorite_posts' : ''}}"><i class="ion-heart"></i>{{ $post->favorite_to_users->count() }}</a>
+                                                   class="{{ !Auth::user()->favorite_posts->where('pivot.post_id',$randompost->id)->count()  == 0 ? 'favorite_posts' : ''}}"><i class="ion-heart"></i>{{ $randompost->favorite_to_users->count() }}</a>
 
                                                 <form id="favorite-form-{{ $post->id }}" method="POST" action="{{ route('post.favorite',$post->id) }}" style="display: none;">
                                                     @csrf
                                                 </form>
                                             @endguest
                                         </li>
-                                        <li><a href="#"><i class="ion-chatbubble"></i>{{$post->comments->count()}}</a></li>
-                                        <li><a href="#"><i class="ion-eye"></i>{{$post->view_count}}</a></li>
+                                        <li><a href="#"><i class="ion-chatbubble"></i>{{$randompost->comments->count()}}</a></li>
+                                        <li><a href="#"><i class="ion-eye"></i>{{$randompost->view_count}}</a></li>
                                     </ul>
 
                                 </div><!-- blog-info -->
